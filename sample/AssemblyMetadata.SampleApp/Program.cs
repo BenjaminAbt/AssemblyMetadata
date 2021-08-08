c06872ff6351f3737c38864f72319a2563d07bb1
Benjamin Abt <mail@benjamin-abt.com>
2021-08-08 20:48:30 +0200

    Init

---
+++ sample/AssemblyMetadata.SampleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BenjaminAbt.AssemblyMetadata;
+
+namespace BenjaminAbt.AssemblyMetadata.SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AssemblyMetadataInfo.BuildInfo.BuildTimestamp);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tests/AssemblyMetadata.UnitTests/AssemblyMetadataInfoTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using BenjaminAbt.AssemblyMetadata;
+using FluentAssertions;
+using System;
+
+namespace BenjaminAbt.AssemblyMetadata.UnitTests
+{
+    public class AssemblyMetadataInfoTests
+    {
+        [Fact]
+        public void BuildInfoTests()
+        {
+            DateTimeOffset buildOn = DateTimeOffset.ParseExact(AssemblyMetadataInfo.BuildInfo.BuildTimestamp, "o", null);
+
+            AssemblyMetadataInfo.BuildInfo.BuildDateYear.Should().Be(buildOn.Year);
+            AssemblyMetadataInfo.BuildInfo.BuildDateMonth.Should().Be(buildOn.Month);
+            AssemblyMetadataInfo.BuildInfo.BuildDateDay.Should().Be(buildOn.Day);
+
+            AssemblyMetadataInfo.BuildInfo.BuildTimeHour.Should().Be(buildOn.Hour);
+            AssemblyMetadataInfo.BuildInfo.BuildTimeMinute.Should().Be(buildOn.Minute);
+            AssemblyMetadataInfo.BuildInfo.BuildTimeSecond.Should().Be(buildOn.Second);
+        }
+    }
+}
